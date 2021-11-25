c0d95d7b7cb737805269e72e44dd1590241a8681
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-11-26 00:04:24 +0200

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,10 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
   const array1 = [19, 20, 34];
-  const classes = []
+  const classes = [];
   for (const element of array1) {
-   classes.push(new ClassRoom(element));
+      classes.push(new ClassRoom(element));
   }
   return classes;
 }
-  
