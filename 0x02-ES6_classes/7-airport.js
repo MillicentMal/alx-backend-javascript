dc5885104ff197e9c146fa75581d172dfea1c395
MillicentMal <millicentmalinga19@gmail.com>
2021-11-26 21:19:02 +0000

    Task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,37 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  set name(Newname) {
+    if (typeof Newname !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = Newname;
+  }
+
+  displayFullCurrency() {
+    return (`${this.name} (${this.code})`);
+  }
+
+  set code(Newcode) {
+    if (typeof Newcode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = Newcode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+}
