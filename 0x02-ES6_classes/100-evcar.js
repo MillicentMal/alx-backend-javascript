2622700bf752a742636bf11d5ada5fc355937e66
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-11-27 01:53:00 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -7,8 +7,8 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    const carClone = (this.constructor[Symbol.species] || this.constructor)();
+    const carClone = (this.constructor[Symbol.species] || this.constructor);
     const = carObj = new carClone();
-    return carClone;
+    return carObj;
   }
 }
