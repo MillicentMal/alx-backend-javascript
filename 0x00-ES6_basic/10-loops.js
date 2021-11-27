bddc73d35e33a6e1a5f79bedf4b5da4ff0fb0eae
MillicentMal <millicentmalinga19@gmail.com>
2021-11-27 19:38:36 +0000

    Fixing linting errors

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newItem = [];
-   for (const idx of array) {
-  newItem.push(appendString + idx);
-
+  for (const idx of array) {
+    newItem.push(appendString + idx);
   }
 
   return newItem;
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments() {
+    getNumberOfDepartments(employeeList) {
       return Object.keys(employeeList).length;
     },
   };
