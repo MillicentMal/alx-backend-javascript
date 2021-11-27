9ae5f9f0c6986f5e48f0503f4b9a7e780c5f3350
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-11-27 17:05:47 +0200

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  
+  // using computed property names
   return {
  [`income-${getCurrentYear()}`] = income,
  [`gdp-${getCurrentYear()}`] = gdp,
- [`capita-${getCurrentYear()}`] = capita
+ [`capita-${getCurrentYear()}`] = capita,
   };
 
 }
