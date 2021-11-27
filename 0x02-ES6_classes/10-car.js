7d601de9a563a3fbb7d01a444555a5145f189461
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-11-27 02:32:12 +0200

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -4,18 +4,10 @@ export default class Car {
     this._motor = motor;
     this._color = color;
   }
-
-<<<<<<< HEAD
   cloneCar() {
-    const carClone = this.constructor[Symbol.species] || this.constructor;
-    const carObj = new carClone();
-    return carObj;
-  }
-=======
-   cloneCar() {
      const Carclone = this.constructor[Symbol.species] || this.constructor;
     const carObj = new Carclone();
      return carObj;
    }
->>>>>>> a66e9d1b5dcd87faf1c3405deeb57200f2ee75f9
+
 }
