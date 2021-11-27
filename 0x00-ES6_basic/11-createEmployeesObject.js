dbc7b29b7cf0117fc7332c2d9b66a2415e00883e
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-11-27 19:05:09 +0200

    Create 11-createEmployeesObject.js

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,6 @@
+export default function createEmployeesObject(departmentName, employees) {
+// a function named createEmployeesObject that will receive two arguments:
+// departmentName (String)
+// employees (Array of Strings)
+return {`$departmentName : [...employees ]`};
+}
