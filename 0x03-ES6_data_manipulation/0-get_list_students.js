edc418775a7e1eac6f3544d1dc560bbc923916e9
MillicentMal <millicentmalinga19@gmail.com>
2021-11-30 10:15:26 +0000

    Task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,9 @@
-export default function getListStudents() {
-<<<<<<< HEAD
-  return;
-  {
-    [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-  }
-=======
-  return 
-    [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco'}, 
-    {id: 2, firstName:'James', location: 'Columbia'},
-    {id: 5, firstName: 'Serena', location: 'San Francisco'}
-    ]
-
->>>>>>> a3e80bf1f27e2bd165cde8ca42c6c682bff5fe6d
+export default function getListStudents() export default function getListStudents() {
+  return  {
+  [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
 }
