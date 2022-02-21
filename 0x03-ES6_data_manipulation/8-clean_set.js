158f92bf544b19dbf895e3b79766641d333647bd
MillicentMal <millicemalinga19@gmail.com>
2022-02-21 21:10:47 +0000

    Task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(_set, startString) {
+	let matchedStrings = [];
+	if (startString.length === 0){
+		return '';
+	}
+	if (typeof _set !== "object" || typeof startString !== 'string') {
+		return '';
+	}
+	let Strings = [..._set].filter(x => x.startsWith(startString));
+	Strings.forEach(x => matchedStrings.push(x.replace(startString, '')));
+	return matchedStrings.join('-');
+	
+	}
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
